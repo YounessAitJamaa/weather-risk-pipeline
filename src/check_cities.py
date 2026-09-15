e47f13590742965a9307fe954d958ef4f94e6272
Youness Ait Jamaa <aitjamaayouness1@gmail.com>
2026-09-15 13:38:14 +0100

    Add weather data extraction to Bronze

--- src/check_cities.py
+++ src/check_cities.py
@@ -1,6 +1,5 @@
 import pandas as pd
 
-
 df = pd.read_csv("data/bronze/cities.csv")
 
 
---
+++ src/extract_weather.py
@@ -0,0 +1,49 @@
+from pathlib import Path
+import pandas as pd
+import requests
+import json
+
+
+BASE_DIR = Path(__file__).resolve().parent.parent
+
+
+API_URL = "https://api.open-meteo.com/v1/forecast"
+
+def get_meteo(city, latitude, longitude):
+    params = {
+        "latitude": latitude,
+        "longitude": longitude,
+        "daily": [
+            "temperature_2m_max",
+            "temperature_2m_min",
+            "precipitation_sum",
+            "precipitation_probability_max",
+            "wind_speed_10m_max",
+            "wind_gusts_10m_max",
+            "weather_code"
+        ],
+        "timezone": "Africa/Casablanca",
+        "forecast_days" : 7
+    }
+
+    response = requests.get(API_URL, params=params, timeout=10)
+
+    response.raise_for_status()
+
+    data = response.json()
+
+    save_files_path = BASE_DIR/"data"/"bronze"/"weather"
+
+    with open(save_files_path / f"{city}.json", "w") as f:
+        json.dump(data, f)
+
+    return data    
+
+
+csv_path = BASE_DIR/"data"/"bronze"/"cities.csv"
+
+
+df = pd.read_csv(csv_path)
+
+for index, row in df.head(2).iterrows(): 
+    get_meteo(row["city"], row["lat"], row["lng"])
